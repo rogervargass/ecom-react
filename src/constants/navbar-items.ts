c526f7a391582fed65872660a6311f59e652b771
Roger Vargas <rogerv@abcbrasil.com.br>
2024-08-22 16:51:04 -0300

    refactor: refactor navbar logic

---
+++ src/constants/navbar-items.ts
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    label: 'Home',
+    url: '/'
+  },
+  {
+    label: 'Collection',
+    url: '/collection'
+  },
+  {
+    label: 'About',
+    url: '/about'
+  },
+  {
+    label: 'Contact',
+    url: '/contact'
+  }
+]
